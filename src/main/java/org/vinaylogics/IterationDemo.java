a4892678b9fae4c2a9106df328c88d1d4c3bb136
Vinayagam <vinayganeshdxj@gmail.com>
2024-04-22 20:38:04 +0530

    Added the return type information

--- src/main/java/org/vinaylogics/IterationDemo.java
+++ src/main/java/org/vinaylogics/IterationDemo.java
@@ -2,7 +2,9 @@ package org.vinaylogics;
 
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.typeutils.TupleTypeInfo;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.IterativeStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -13,7 +15,8 @@ public class IterationDemo {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         DataStream<Tuple2<Long, Integer>> dataStream = env.generateSequence(0,5)
-                .map((MapFunction<Long, Tuple2<Long, Integer>>) value -> new Tuple2<>(value, 0));
+                .map((MapFunction<Long, Tuple2<Long, Integer>>) value -> new Tuple2<>(value, 0))
+                .returns(new TupleTypeInfo<>(Types.LONG, Types.INT));
 
         // Prepare stream for iteration
         IterativeStream<Tuple2<Long, Integer>> iteration = dataStream.iterate(5000);
@@ -25,17 +28,17 @@ public class IterationDemo {
                 return value;
             else
                 return new Tuple2<>(value.f0+1, value.f1 +1);
-        }); // plusone 1,1 2,1 3,1 4,1 5,1 6,1
+        }).returns(new TupleTypeInfo<>(Types.LONG, Types.INT)); // plusone 1,1 2,1 3,1 4,1 5,1 6,1
 
         // part of stream used in next iteration
         DataStream<Tuple2<Long, Integer>> notEqualToTen = plusOne.filter(
-                (FilterFunction<Tuple2<Long, Integer>>) value -> value.f0 != 10);
+                (FilterFunction<Tuple2<Long, Integer>>) value -> value.f0 != 10).returns(new TupleTypeInfo<>(Types.LONG, Types.INT));
 
         // feedback to next iteration
         iteration.closeWith(notEqualToTen);
 
         DataStream<Tuple2<Long, Integer>> equalToTen = plusOne.filter(
-                (FilterFunction<Tuple2<Long, Integer>>) value -> value.f0 != 10);
+                (FilterFunction<Tuple2<Long, Integer>>) value -> value.f0 != 10).returns(new TupleTypeInfo<>(Types.LONG, Types.INT));
 
         equalToTen.writeAsText("/home/vinay/ten");
 
